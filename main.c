e2e4b73f3443858be00becbc9c9ecf2953da2396
劉俊廷 <M10407209@mail.ntust.edu.tw>
2015-07-15 20:06:00 +0800

    new project

---
+++ main.c
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.c
+ * Author: david
+ *
+ * Created on 2015年7月15日, 下午 3:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hello\n");
+ 
+ 
+
+
+ 
+    return (EXIT_SUCCESS);
+}
+
